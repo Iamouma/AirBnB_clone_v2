2b1da4bc38fd8ce2e439c946451084619aa00ad2
Iamouma <stephenouma452@gmail.com>
2024-04-04 12:36:39 -0700

    A Fabric script (based on the file 3-deploy_web_static.py) that deletes out-of-date archives, using the function do_clean

---
+++ 100-clean_web_static.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""
+The fabfile distributes an archive to web servers
+"""
+
+import os
+from fabric.api import *
+from datetime import datetime
+
+
+# This sets the IP addresses for web-01 && web-02
+env.hosts = ['52.90.0.78', '3.85.196.138']
+env.user = "ubuntu"
+
+
+def do_pack():
+    """Archives the static files"""
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    archive_path = "versions/web_static_{}.tgz".format(now)
+
+    local("mkdir -p versions")
+
+    archived = local("tar -cvzf {} web_static".format(archive_path))
+
+    if archived.return_code != 0:
+        return None
+    else:
+        return archive_path
+
+
+def do_deploy(archive_path):
+    """This uses os module to check for valid file path"""
+    if os.path.exists(archive_path):
+        archive = archive_path.split('/')[1]
+        a_path = "/tmp/{}".format(archive)
+        folder = archive.split('.')[0]
+        f_path = "/data/web_static/releases/{}/".format(folder)
+
+        put(archive_path, a_path)
+        run("mkdir -p {}".format(f_path))
+        run("tar -xzf {} -C {}".format(a_path, f_path))
+        run("rm {}".format(a_path))
+        run("mv -f {}web_static/* {}".format(f_path, f_path))
+        run("rm -rf {}web_static".format(f_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(f_path))
+        return True
+    return False
+
+
+def deploy():
+    """
+    This create and archive and get its path
+    """
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
+
+def do_clean(number=0):
+    """Deletes out-of-date archives of the static files.
+    Args:
+        number (Any): The number of archives to keep.
+    """
+    archives = os.listdir('versions/')
+    archives.sort(reverse=True)
+    start = int(number)
+    if not start:
+        start += 1
+    if start < len(archives):
+        archives = archives[start:]
+    else:
+        archives = []
+    for archive in archives:
+        os.unlink('versions/{}'.format(archive))
+    cmd_parts = [
+        "rm -rf $(",
+        "find /data/web_static/releases/ -maxdepth 1 -type d -iregex",
+        " '/data/web_static/releases/web_static_.*'",
+        " | sort -r | tr '\\n' ' ' | cut -d ' ' -f{}-)".format(start + 1)
+    ]
+    run(''.join(cmd_parts))
